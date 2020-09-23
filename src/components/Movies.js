1951b5ca8b7cc06ca3dfb72a83fb915e905cd501
Ceddyp19 <ceddyp117@gmail.com>
2020-09-23 17:20:12 -0500

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import { movies } from '../data';
-
+//title, time, genre, metascore
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+      {movies.map(movie => (
+        <div key={movie.title}>
+          <h3>Name: {movie.title}</h3>
+          <p>Time: {movie.time}</p>
+          <p>Genre:</p>
+          <ul>
+            {movie.genres.map(genre => ( <li> {genre} </li> ))}
+          </ul>
+        </div>
+      ))}
     </div>
   );
 };
